a09dda6e5e2b110d5f39f94831f2e47d7768da3e
WyattAlexander-001 <wyattbushman117@gmail.com>
2023-10-23 20:32:30 -0400

    finished basic app

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const franc = require('franc');
 const langs = require('langs');
+const colors = require('colors');
+const input = process.argv[2];
 
 //test franc:
-const langCode = (franc('Alle menslike wesens word vry')); // 'afr' Afrikaans
-console.log(franc('এটি একটি ভাষা একক IBM স্ক্রিপ্ট')) //=> 'ben'
-console.log(franc('Alle menneske er fødde til fridom')) //=> 'nno'
+// const langCode = (franc('Alle menslike wesens word vry')); // 'afr' Afrikaans
+// console.log(franc('এটি একটি ভাষা একক IBM স্ক্রিপ্ট')) //=> 'ben'
+// console.log(franc('Alle menneske er fødde til fridom')) //=> 'nno'
 
+const langCode = franc(input);
 const language = langs.where("3", langCode);
-console.log(language.name); //=> 'Afrikaans'
+
+if (language === undefined) {
+    console.log("Sorry, couldn't figure it out. Try with more sample text.");
+}else{
+    console.log(`We guess that language is: ${language.name}.`.rainbow);
+}
+
+// node index.js 'Alle menneske er fødde til fridom'
